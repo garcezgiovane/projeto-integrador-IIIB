33ce7dfb03e894f7fbd2d99324aadd69c60222a8
Dudis-hub <64049752+Dudis-hub@users.noreply.github.com>
2020-05-24 19:34:06 -0300

    novo

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,5 +1,174 @@
-import React from "react";
+import React, {useState} from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import { Link } from "react-router-dom";
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import api from "../../service/api";
+
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+     
+      Desenvolvido por WhateverTech
+      
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 export default function SignUp() {
-  return <h1>REGISTRO</h1>;
+
+  const classes = useStyles();
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
+  const [cpassword, setCpassword] = useState("");
+  const [email, setEmail] = useState("");
+  
+  
+  async function register(e) { 
+    e.preventDefault() 
+
+    console.log("register") 
+    const obj = { username: user, password, email, cpassword }; 
+if(user == '' || password == '' || email == ''){
+  
+  alert('Todos os campos devem estar preenchidos')
+  return false; 
+  console.log(password)
+console.log(cpassword)
+  if(password === cpassword){
+    alert('As senhas não correspondem')
+    return false; }
+
+}
+    const response = await api.post("/register", obj);
+  console.log(response)
+  }
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Cadastre-se
+        </Typography>
+        <form className={classes.form} noValidate onsubmit="return validar()">
+        <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                autoComplete="fname"
+                name="username"
+                variant="outlined"
+                required
+                fullWidth
+                id="username"
+                label="Nome de Usuário"
+                autoFocus
+                onChange={(e) => setUser(e.target.value)}
+              />
+             
+           
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="E-mail"
+                name="email"
+                autoComplete="email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Senha"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+        
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="cpassword"
+                label="Confirme a senha"
+                type="password"
+                id="cpassword"
+                autoComplete="current-password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+        
+            </Grid>
+            
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            onClick={(e) => register(e)}
+          >
+            Cadastre-se 
+          </Button>
+          <Grid container justify="flex-end">
+          <Grid item>
+              <Link to="/" variant="body2">
+                {"Já possui uma conta? Conecte-se"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={5}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
 }
